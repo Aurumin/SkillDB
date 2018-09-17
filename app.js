6434bd935e0e4faf75a724045bf77d59b9f1e8dc
Alexander Böckle <alexander.boeckle@code.berlin>
2018-09-17 15:45:37 +0200

    Mongoose Connection with external configuration

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
+// External dependencies
 const express = require("express");
+const mongoose = require("mongoose");
+
+// Internal dependencies
+const keys = require("./config/keys");
+
+// Connect to the database
+mongoose.connect(keys.mongoURL);
 
 // Start app
 const app = express();
